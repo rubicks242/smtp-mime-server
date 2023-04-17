eb8a98703604ea208c2660b7c1118adc5526d064
mssandeepkamath <msandeepcip@gmail.com>
2023-04-17 17:01:20 +0530

    try

--- src/main/java/com/example/studentassistancehelperapi/Service/MailService.java
+++ src/main/java/com/example/studentassistancehelperapi/Service/MailService.java
@@ -5,6 +5,7 @@ import com.example.studentassistancehelperapi.Model.Response;
 
 import java.util.Map;
 
+//this is an interface
 public interface MailService {
 
      Response sendEmail(Mail request, Map<String, Object> model,String to);
